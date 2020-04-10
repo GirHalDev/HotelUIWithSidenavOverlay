1a7b115def1012df9a050be3726e2626d80acbff
Tsion Zewdie <PommyTsi@Tsions-MacBook-Air.local>
2020-04-10 11:34:43 -0500

    Hotel UI with sidenav overlay

---
+++ Script/mobile.js
@@ -0,0 +1,11 @@
+// set the width of the side navigation to 250px
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+// set the width of the side navigation to o
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+      
+}
+
